5e5d96ffb5a907ffe28c0aa45e61010da8cbd01c
Igor <scratchflow@krawczuk.eu>
2016-09-17 18:12:05 +0200

    only übergabe missing

--- GraphArea.elm
+++ GraphArea.elm
@@ -8,6 +8,7 @@ import Html.Events exposing (on)
 import Json.Decode as Json exposing ((:=))
 import Mouse exposing (Position)
 import Task
+import Dict
 
 
 -- IMPORT MYMODULES
@@ -18,33 +19,33 @@ type Msg = AddNode GraphicalNode.Model
     | NodeUpdate GraphicalNode.Msg
     | ChangeOffset Int
 
-type alias Model= {nodes:List GraphicalNode.Model, offset:Int}
+type alias Model= {nodes:Dict.Dict Int GraphicalNode.Model, offset:Int, id:Int}
 
 view : Model -> Html Msg
 view model =
     div [style (graphAreaStyle model.offset)]
-    ([text "Grapharea"]++(List.map (\n -> renderNode n) model.nodes))
+    ([text "Grapharea"]++(List.map (\n -> renderNode n) (Dict.values model.nodes)))
 
 update: Msg->Model-> (Model, Cmd Msg)
 update msg model=
     case msg of
         ChangeOffset w -> ({model|offset = w},Cmd.none)
         NodeUpdate newnodemodel -> (model,Cmd.none)
-        AddNode newnode -> (addNode model newnode, Cmd.none)
+        AddNode newnode -> ((addNode model newnode), Cmd.none)
 
 addNode:Model->GraphicalNode.Model->Model
 addNode model nn=
-    let oldnodes=model.nodes in
-            {model|nodes=List.append oldnodes [nn]}
+    let newid=model.id+1 in 
+    let newnodes=(Dict.insert newid nn model.nodes) in
+            {model|nodes=newnodes,id=newid}
 
-renderNode:GraphicalNode.Model-> Html msg
-renderNode node = text "muh"
-        
+renderNode:GraphicalNode.Model-> Html Msg
+renderNode node = App.map NodeUpdate (GraphicalNode.view node)
 
 graphareasubs = []
 subscriptions: Model -> Sub Msg
 subscriptions model = 
-    Sub.batch ((List.map (\n-> Sub.map NodeUpdate (GraphicalNode.subscriptions n)) model.nodes)++graphareasubs)
+    Sub.batch ((List.map (\n-> Sub.map NodeUpdate (GraphicalNode.subscriptions n)) (Dict.values model.nodes))++graphareasubs)
 
 
 graphAreaStyle:Int -> List(String,String)
